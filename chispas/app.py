640ae007945a61b450a4ab7dc6e7a8621bfced64
Ryan Perry <rperry2174@gmail.com>
2023-09-18 23:51:11 -0400

    Add variables to test other languages

--- chispas/app.py
+++ chispas/app.py
@@ -59,17 +59,21 @@ def create_app():
         initial_difficult_words = get_unknown_words(user_id)
         difficult_words = " ".join(initial_difficult_words)
 
+        # Add constants for testing (so we can test other languages too)
+        LEARNING_LANGUAGE="spanish"
+        KNOWN_LANGUAGE="english"
+
         # Analyze the themes in the list of unknown words using ChatGPT
-        themes = analyze_themes_with_chatgpt(initial_difficult_words)
+        themes = analyze_themes_with_chatgpt(initial_difficult_words, learning_language=LEARNING_LANGUAGE, known_language=KNOWN_LANGUAGE)
 
         # Generate detailed explanations for the unknown words
-        theme_details = generate_detailed_explanations(initial_difficult_words, themes)
+        theme_details = generate_detailed_explanations(initial_difficult_words, themes, learning_language=LEARNING_LANGUAGE, known_language=KNOWN_LANGUAGE)
 
         # Generate a new example paragraph incorporating the unknown words
-        new_example = generate_new_examples(initial_difficult_words, themes)
+        new_example = generate_new_examples(initial_difficult_words, themes, learning_language=LEARNING_LANGUAGE, known_language=KNOWN_LANGUAGE)
 
         # Generate a new text block for the next learning cycle
-        progressive_text = generate_progression_text_block(initial_difficult_words, themes, "beginner")
+        progressive_text = generate_progression_text_block(initial_difficult_words, themes, "beginner", learning_language=LEARNING_LANGUAGE, known_language=KNOWN_LANGUAGE)
 
         return render_template('index.html', random_text=random_text, difficult_words=difficult_words, themes=themes, theme_details=theme_details, new_example=new_example, progressive_text=progressive_text)
 
